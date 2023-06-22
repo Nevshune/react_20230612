7ae19a28b5633d5e604bcbea84f2dbe58d1ddd2b
Nevshune <kangdon03@gmail.com>
2023-06-22 10:57:15 +0900

    20230622

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
 import { Box, Button, HStack, Image, Stack, Text, useColorMode } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs"
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
-    const [scroll, setScroll] =useState(true)
-    const [winscroll, setWinscroll] =useState(true)
+    const [scroll, setScroll] = useState(true)
+    const [winscroll, setWinscroll] = useState(true)
     useEffect(() => {
         document.addEventListener("wheel", (event) => {
-            if (event.deltaY < 0) { 
+            if (event.deltaY < 0) {
                 setScroll(true);
-            } else if (event.deltaY > 0) { 
-                setScroll(false);                
+            } else if (event.deltaY > 0) {
+                setScroll(false);
             };
 
-            if (event.deltaY < 0) { 
+            if (event.deltaY < 0) {
                 setWinscroll(true);
-            } else if (event.deltaY > 0) { 
-                setWinscroll(false);                
+            } else if (event.deltaY > 0) {
+                setWinscroll(false);
             }
         })
     })
@@ -29,13 +29,18 @@ export default function Header() {
         { title: "events", href: "/events" }
     ]
 
+    const location = useLocation();
+
+    const link = document.location.href;
+    const array = link.split('3000/')[1]
+    console.log(array)
+
     return <Stack
         bg={winscroll ? "transparent" : "gray.200"}
         transform={scroll ? "translateY(0px)" : "translateY(-60px)"}
         transition="0.3s"
         w="full"
         h="60px"
-        color="white"
         fontWeight={600}
         fontSize="20px"
         alignItems="center"
@@ -49,9 +54,11 @@ export default function Header() {
                 <Box>
                     <Image src="https://1000logos.net/wp-content/uploads/2021/06/Marvel-Comics-logo-500x281.png" alt="Main logo" height="60px"></Image>
                 </Box>
-                <HStack color="white" textTransform="uppercase" spacing={5}>
+                <HStack color="" textTransform="uppercase" spacing={5}>
                     {GNB.map((item) => (
-                        <Link to={item.href} key={item.title} aria-label={item.title}><Text>{item.title}</Text></Link>
+                        <Link to={item.href} key={item.title} aria-label={item.title}>
+                            <Text color={item.href === location.pathname ? "red" : ""}>{item.title}</Text>
+                        </Link>
                     ))}
 
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,43 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, HStack } from "@chakra-ui/react";
 import CarouselSlick from "../components/carouselSilck";
+import CardItems from "../components/carditem";
 
-export default function Home(){
-    return(
-        // 캐러셀
-        <Box h="400vh">
+
+export default function Home() {
+    const featuresLists = [
+        {
+            title: "스마트웹3기",
+            description: "ehdgoanfrhk",
+            buttionText: "자세히보기"
+        },
+
+        {
+            title: "스마트웹3기",
+            description: "ehdgoanfrhk",
+            buttionText: "자세히보기"
+        },
+
+        {
+            title: "스마트웹3기",
+            description: "ehdgoanfrhk",
+            buttionText: "자세히보기"
+        }
+    ];
+
+    return <>
+        <Box>
             <CarouselSlick />
         </Box>
-    )
+
+
+        <HStack justifyContent="center" py="16" bg="gray.100">
+            <Grid gap="4" w="7xl" templateColumns={"repeat(3, 1fr)"}>
+                {
+                    featuresLists.map((item, i) => (
+                        <CardItems key={i} item={item} />
+                    ))
+                }
+            </Grid>
+        </HStack>
+    </>
 }
